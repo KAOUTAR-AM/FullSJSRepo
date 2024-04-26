0c21c2ca80483310a844b1a10ae9219f2781c957
KAOUTAR AMAZZAR <kaoutaramazzar@gmail.com>
2024-04-26 13:35:22 +0100

    new update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-
 import React from 'react';
-import{Footer,Blog,Possibility,Feature,WhatGPT3,Header} from './containers';
-import{Cta,Brand,Navbar} from './componenets';
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 import './App.css';
 
-
-const App = () => {
-  return (
-    <div className='App'>
-      <div className='gradient_bg'>
-        <Navbar/>
-        <Header/>
-      </div> 
-      <div>
-        <Brand/>
-        <WhatGPT3/>
-        <Feature/>
-        <Possibility/>
-        <Cta/>
-        <Blog/>
-        <Footer/>
-      </div>
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-    
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+ import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from './App';
-import './index.css';
+ import App from './App';
+ import './index.css';
 
-//ReactDOM.render(<App/>, document.getElementById('root'));
-createRoot(document.getElementById('root')).render(<App />);
+ //ReactDOM.render(<App />, document.getElementById('root'));
+
+ const root = document.getElementById('root');
+ const rootContainer = ReactDOM.createRoot(root);
+ rootContainer.render(<App />);
