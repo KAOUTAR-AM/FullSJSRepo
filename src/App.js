731e79adf849039215a4077f51a9fc27985d49ca
KAOUTAR AMAZZAR <kaoutaramazzar@gmail.com>
2024-04-26 12:54:38 +0100

    navbar and header update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
+
+import React from 'react';
+import{Footer,Blog,Possibility,Feature,WhatGPT3,Header} from './containers';
+import{Cta,Brand,Navbar} from './componenets';
+import './App.css';
+
 
 const App = () => {
   return (
-    <div>
-        <h1>Hello</h1>
+    <div className='App'>
+      <div className='gradient_bg'>
+        <Navbar/>
+        <Header/>
+      </div> 
+      <div>
+        <Brand/>
+        <WhatGPT3/>
+        <Feature/>
+        <Possibility/>
+        <Cta/>
+        <Blog/>
+        <Footer/>
+      </div>
     </div>
+    
   )
 }
 
